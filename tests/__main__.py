1662befd5df6ed057e44d70431ce64169344a4a1
Johan Hardy <johan.hardy@spacebel.be>
2019-06-22 19:19:17 +0000

    added test placeholders

---
+++ tests/__main__.py
@@ -0,0 +1,6 @@
+"""Import MQTT test cases"""
+import test_mqtt
+
+
+if __name__ == "__main__":
+    test_mqtt.run()
---
+++ tests/test_mqtt.py
@@ -0,0 +1,8 @@
+"""
+This file encloses all the test cases of the MQTT client.
+"""
+
+
+def run():
+    """Run all tests"""
+    print('execute mqtt tests')

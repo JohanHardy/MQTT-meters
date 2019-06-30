a8b4b7dcbfaa2db63fb7bb2064f031d5de7da118
Johan Hardy <johan.hardy@spacebel.be>
2019-06-30 20:17:06 +0000

    added agrs parser

--- __main__.py
+++ __main__.py
@@ -6,15 +6,31 @@ Author: Johan Hardy
 Email: hardy.johan@gmail.com
 """
 import sys
-import water.meter
-import water.tank
-import paho.mqtt.client as mqtt
+import argparse
+#import water.meter
+#import water.tank
+#import paho.mqtt.client as mqtt
+import config
 
 
 def main():
-    for arg in sys.argv[1:]:
-        print(arg)
-
+    ''' Construct the argument parse and parse the arguments '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--version", action='version',
+                        version='%(prog)s 1.0')
+    parser.add_argument("--host", required=False, help="MQTT broker host",
+                        default=config.MQTT_BROCKER_HOST)
+    parser.add_argument("--port", required=False, help="MQTT broker port",
+                        default=config.MQTT_BROCKER_PORT)
+    parser.add_argument('--keep', required=False, help="MQTT broker keepalive",
+                        default=config.MQTT_KEEP_ALIVE)
+    parser.add_argument('--meter', required=True, help="Kind of meter", choices=['tank', 'water'])
+    args = vars(parser.parse_args())
+    # Display a friendly message to the user
+    print("Starting MQTT client {}:{} keepalive {}".format(args["host"],
+                                                           args["port"],
+                                                           args["keep"]))
+    print("Setting up {} meter".format(args["meter"]))
     return 0
 
 
--- config.py
+++ config.py
@@ -1,7 +1,6 @@
 ''' Default configurations '''
 MQTT_BROCKER_HOST = '192.160.0.10'
 MQTT_BROCKER_PORT = 1883
-MQTT_CREDENTIALS = {'username': 'sensors', 'password': 'publish'}
 MQTT_KEEP_ALIVE = 60
 MQTT_TOPIC_WATER_METER = 'sensors/water/meter'
 MQTT_TOPIC_WATER_TANK = 'sensors/water/tank'

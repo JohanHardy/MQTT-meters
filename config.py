8c1b17cc267dbe8212aff1d69ad00a1ceadf8027
Johan Hardy <johan.hardy@spacebel.be>
2019-06-30 09:05:11 +0000

    Created default configuration

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
+''' Default configurations '''
 MQTT_BROCKER_HOST = '192.160.0.10'
 MQTT_BROCKER_PORT = 1883
+MQTT_CREDENTIALS = {'username': 'sensors', 'password': 'publish'}
+MQTT_KEEP_ALIVE = 60
+MQTT_TOPIC_WATER_CONS = 'sensors/water/consumption'
+MQTT_TOPIC_WATER_TANK_LEVEL = 'sensors/water/tank/level'

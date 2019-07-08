add63f9b2d2e6116e456062f36b949003cd0aabf
Johan Hardy <johan.hardy@spacebel.be>
2019-07-08 09:26:55 +0000

    fixed configuration

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 ''' Default configurations '''
-MQTT_BROCKER_HOST = '192.160.0.10'
+MQTT_BROCKER_HOST = '192.168.0.10'
 MQTT_BROCKER_PORT = 1883
 MQTT_KEEP_ALIVE = 60
 MQTT_TOPIC_WATER_METER = 'sensors/water/meter'
